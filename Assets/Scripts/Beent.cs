5659eb91b854260b63cadb21d3679fcefde04dc7
Leeman <leemanonline@gmail.com>
2023-11-02 15:54:04 -0400

    Update Beent.cs
    better beent interaction with state design pattern

--- Assets/Scripts/Beent.cs
+++ Assets/Scripts/Beent.cs
@@ -2,23 +2,41 @@ using UnityEngine;
 public abstract class Beent : MonoBehaviour
 {
     #region Attributes
-    protected virtual int currentHealth { get; set; }
-    protected virtual State currentState { get; set; }
-    protected abstract State reactionState { get; set; }
+    #region Personal Attributes
+    protected virtual int CurrentHealth { get; set; }
+    protected virtual int MoveSpeed { get; set; }
+    #endregion
+    #region State Machine Attributes
+    protected virtual State CurrentState { get; set; }
+    #endregion
     #endregion
     #region Operations
+    #region Unity Operations
+    protected virtual void Update()
+    {
+        if (CurrentState != null) CurrentState.UpdateState();
+        DoSenses();
+    }
+    #endregion
+    #region Local Operations
     protected virtual float CheckPopulation()
     { // get a count of all objects tagged with Beent
         return GameObject.FindGameObjectsWithTag("Beent").Length;
     }
     public virtual void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        if (currentHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0) Destroy(gameObject);
     }
-    protected abstract void DoStateStuff(State currentState);
+    #endregion
+    #region State Machine Operations
+    protected virtual void ChangeState(State newState)
+    {
+        if (CurrentState != null) CurrentState.ExitState();
+        CurrentState = newState;
+        CurrentState.EnterState();
+    }
+    protected abstract void DoSenses(); // check events and trigger transitions
+    #endregion
     #endregion
 }
