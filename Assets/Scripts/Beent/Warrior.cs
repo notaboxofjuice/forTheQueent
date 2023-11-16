3494814786de20290edb4ebd8550c76924aa4f06
NSCarlisle <nathan.carlisle95@gmail.com>
2023-11-16 16:31:15 -0500

    Chase State
    Wrote a "Chase State" that warriors will enter upon detecting an enemy in their perimeter

--- Assets/Scripts/Beent/Warrior.cs
+++ Assets/Scripts/Beent/Warrior.cs
@@ -4,42 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Warrior : Beent
 {
-    Vector3 currentTarget;
-    bool isChasing = false;
-    public List<Node> currentPath;
-    [Tooltip("Close Approach Distance that stops chase when reached.")]
-    [SerializeField] float closeApproach = 1f;
+    protected Vector3 currentTarget;
     protected override void DoSenses() // look for events and trigger transitions
     {
         
-    }
-    IEnumerator ChaseTarget()
-    {
-        yield return new WaitForFixedUpdate();
-        if (isChasing)
-        {
-            pathfinder.CalculatePath(transform.position, currentTarget);
-            if(pathfinder.lastFoundPath != null )
-            {
-                transform.position = Vector3.MoveTowards(transform.position, pathfinder.lastFoundPath[1].worldCoords, MoveSpeed * Time.deltaTime);
-            }  
-            float distanceToTarget = Vector3.Distance(transform.position, currentTarget);
-            if(distanceToTarget <= closeApproach)
-            {
-                isChasing = false;
-                StopCoroutine(ChaseTarget());
-            }        
-            StartCoroutine(ChaseTarget());
-        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Enemy"))
         {
             currentTarget = other.transform.position;
-            isChasing = true;
+            ChangeState(gameObject.GetComponent<Chase>());
             Debug.Log("New Enemy Found");
-            StartCoroutine(ChaseTarget());
         }
+    }
+    public Vector3 GetCurrentTarget()
+    { 
+        return currentTarget;
+    }
+    public float GetMoveSpeed()
+    {
+        return MoveSpeed;
     }
 }
---
+++ Assets/Scripts/States/Warrior/Chase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : State
+{
+    float MoveSpeed;
+    Vector3 target;
+    [Tooltip("Close Approach Distance that stops chase when reached.")]
+    [SerializeField] float closeApproach = 1f;
+    public override void EnterState()
+    {
+        MoveSpeed = gameObject.GetComponent<Warrior>().GetMoveSpeed();
+    }
+    public override void ExitState()
+    {
+        StopAllCoroutines();
+    }
+    public override void UpdateState()
+    {
+        StartCoroutine(ChaseTarget());
+    }
+    protected override Vector3 ChooseDestination()
+    {
+        Warrior warrior = gameObject.GetComponent<Warrior>();
+        target = warrior.GetCurrentTarget();
+        return target;
+    }
+    IEnumerator ChaseTarget()
+    {
+        yield return new WaitForFixedUpdate();
+        MyPathfinder.CalculatePath(transform.position, ChooseDestination());
+        if (MyPathfinder.lastFoundPath != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MyPathfinder.lastFoundPath[1].worldCoords, MoveSpeed * Time.deltaTime);
+        }
+        float distanceToTarget = Vector3.Distance(transform.position, target);
+        if (distanceToTarget <= closeApproach)
+        {
+            StopCoroutine(ChaseTarget());
+            //Exit chase state here
+        }
+    }
+}
