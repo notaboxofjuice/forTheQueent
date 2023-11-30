9a01f0f995fabedcbfc21e4ed2736452e7996843
Leeman <leemanonline@gmail.com>
2023-11-29 21:15:28 -0500

    Update State.cs
    comments

--- Assets/Scripts/States/State.cs
+++ Assets/Scripts/States/State.cs
@@ -10,9 +10,9 @@ public abstract class State : MonoBehaviour
         daddy = GetComponent<Beent>(); // Get Beent
         myAgent = GetComponent<NavMeshAgent>(); // Get NavMeshAgent
     }
-    public abstract void EnterState(); // Called when a state is first entered, include logic for assigning variables and other initialization things
+    public abstract void EnterState(); // Called when a state is first entered, initialization things
     public abstract void UpdateState(); // The states main functionality whether that be fleeing, processing, etc
-    public virtual void ExitState() // Any cleanup you want to do before exiting a state
+    public virtual void ExitState() // Any cleanup before exiting a state. Remember to do base.ExitState() if you override this method
     {
         daddy.CurrentState = null;
     }
