915f30276e2b46a455b589963d0d8fb3db1d02fc
Leeman <leemanonline@gmail.com>
2023-12-02 13:41:33 -0500

    pollen attempts

--- Assets/Scripts/Pollen.cs
+++ Assets/Scripts/Pollen.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 public class Pollen : MonoBehaviour
 {
+    private void Start()
+    {
+        Invoke("DestroyPollen", 10);
+    }
+    private void DestroyPollen()
+    {
+        Destroy(gameObject);
+    }
     private void OnDestroy()
     {
         PollenFactory.PollenList.Remove(gameObject);
